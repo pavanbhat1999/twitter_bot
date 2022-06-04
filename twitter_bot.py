bafc58914ad8a5fabbf8af28ec4aaf51ae76008b
pavanbhat1999 <prbhat07@gmail.com>
2022-06-04 20:26:40 +0530

    working but not able to get auth

--- twitter_bot.py
+++ twitter_bot.py
@@ -2,6 +2,24 @@ from lib2to3.pgen2 import token
 import tweepy
 from get_quotes import get_quotes
 
+# define all variables 
+ACCESS_TOKEN = "1478947519212122114-YBrdfURFpsEB4eQ38pZH1PBhvtVSx9";
+ACCESS_TOKEN_SECRET = "arLsvLJT7eDafUQO2SUesYGmm5lIDPZ3Cp6g19hiHaxx9";
+API_KEY="O8HdUp0JwJ4fkozxwDjiVWaKZ";
+API_KEY_SECRET="XExMWueyXcfKxB7KWA2oselq6NV050SHYl96IFNUh0qp9kHT1b";
+
+CONSUMER_KEY = "GYSDpAs24mst1paRiT9PQIcf8";
+CONSUMER_SECRET = "7KQuPfh2a7C85TKA0NSMm6dfnLqJ6ewMmurO27zOnSzy1JrlTk";
+CLIENT_ID ="UldHcmt4bV9OdUFabWU3c29IY0Y6MTpjaQ";
+CLIENT_SECRET="34vaoiCZAs6akJNx2Lb2KnbQ1ReT9pX8I2vdn0AZanvmBVw6co";
+
+# Authenticate to Twitter
+auth = tweepy.OAuthHandler(API_KEY,API_KEY_SECRET)
+auth.set_access_token(ACCESS_TOKEN,ACCESS_TOKEN_SECRET)
+
+api = tweepy.API(auth)
+
+api.update_status("Hello Tweepy")
 
 def tweet_bot():
     quote = get_quotes()
@@ -10,39 +28,3 @@ def tweet_bot():
 
 
 tweet_bot();
-
-
-
-
-
-
-
-
-
-
-
-# # Authenticate to Twitter
-# auth = tweepy.OAuthHandler("Ln9iLKOYbGlAODjByLLjcOBpe",
-#     "HJt5EDJDFSZG9D1Iu1njMaYL8QN0jsn1QfEYRnUQFUovU9TPnr")
-# auth.set_access_token("1478947519212122114-44kO8zx980E6kCfANBco3XuEoDwUe4", 
-#     "iQFap825Z5pAnGdHQDgMGRcbuwae9HoCaS4iD1u8t9k0p")
-
-# api = tweepy.API(auth)
-
-# try:
-#     api.verify_credentials()
-#     print("Authentication OK")
-# except:
-#     print("Error during authentication")
-
-
-
-  
-
-#     # access token
-#     # 1478947519212122114-44kO8zx980E6kCfANBco3XuEoDwUe4
-#     # access token secret 
-#     # iQFap825Z5pAnGdHQDgMGRcbuwae9HoCaS4iD1u8t9k0p
-
-#     # bearer token 
-#     # AAAAAAAAAAAAAAAAAAAAAFIvdQEAAAAA%2Fb2c5dcFzI0Df3yKHCmtEF09Zj0%3D2nH38geD0yY2QadJWO4hjpli5dVEbuOAw7ZEebjeOQSTDH2vkw
